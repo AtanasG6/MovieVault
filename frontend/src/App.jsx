26c0b8c907a9bbd4a2efa87af4831f3302fbe599
AtanasG6 <gyulchev666@gmail.com>
2026-02-18 21:59:35 +0200

    Add conditional rendering for movie cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,37 @@ import "./App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
+  const movieNumber = 1;
+
   return (
     <>
-      <MovieCard
-        movie={{
-          title: "The Shawshank Redemption",
-          release_date: "1994-09-23",
-          url: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
-        }}
-      />
+      {movieNumber === 1 ? (
+        <MovieCard
+          movie={{
+            title: "The Shawshank Redemption",
+            release_date: "1994-09-23",
+            url: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
+          }}
+        />
+      ) : (
+        <MovieCard
+          movie={{
+            title: "The Lord of the Rings: The Return of the King",
+            release_date: "2003-12-17",
+            url: "https://m.media-amazon.com/images/I/51Qvs9i5a%2BL._AC_.jpg",
+          }}
+        />
+      )}
+
+      {/* {movieNumber === 1 && (
+        <MovieCard
+          movie={{
+            title: "The Lord of the Rings: The Return of the King",
+            release_date: "2003-12-17",
+            url: "https://m.media-amazon.com/images/I/51Qvs9i5a%2BL._AC_.jpg",
+          }}
+        />
+      )} */}
     </>
   );
 }
